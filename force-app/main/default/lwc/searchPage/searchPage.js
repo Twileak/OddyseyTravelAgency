f7122eb32b1133af8e01ca0cf4de4bec4d2455ae
Dawid Żamojda <dawidzamojdamail@wp.pl>
2023-07-19 09:56:10 +0200

    Added every needed content

---
+++ force-app/main/default/lwc/searchPage/searchPage.js
@@ -0,0 +1,26 @@
+import { LightningElement, wire, track } from 'lwc';
+import getAllProducts from '@salesforce/apex/ProductController.getAllProducts';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class SearchPage extends LightningElement {
+    @track products;
+    @track selectedProduct;
+
+    @wire(getAllProducts)
+    wiredAllProducts(value){
+        this.results = value;
+        const{data, error} = value;
+        if(data){
+            this.products = JSON.parse(data);
+            console.log('2');
+        }else if(error){
+            const event = new ShowToastEvent({
+                title: 'No Results',
+                message: 'We found no results',
+                variant: 'info'
+            });
+            this.dispatchEvent(event);
+            return;
+        }
+    }
+}
