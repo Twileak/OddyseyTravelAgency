ac5e7ef5fe12ace188a60effad2422f03853bebb
Dawid Żamojda <dawidzamojdamail@wp.pl>
2023-07-26 13:29:44 +0200

    Finished product managment aura component

--- force-app/main/default/aura/ProductManagement/ProductManagementController.js
+++ force-app/main/default/aura/ProductManagement/ProductManagementController.js
@@ -1,4 +1,16 @@
 ({
+    showEditModal: function(component, event, helper) {
+            var target = event.currentTarget;
+            var idOfRecord = target.dataset.recordid;
+            console.log('value: ', idOfRecord);
+            component.set("v.selectedRecordId", idOfRecord);
+            component.set("v.showModal", true);
+        },
+
+    handleEditCancel: function(component, event, helper) {
+             component.set("v.showModal", false);
+         },
+
     loadProducts: function(component, event, helper) {
             var action = component.get("c.getProducts");
             action.setCallback(this, function(response) {
@@ -12,41 +24,52 @@
             $A.enqueueAction(action);
         },
 
-    editProduct: function(component, event, helper) {
-        var productId = event.currentTarget.dataset.recordId;
-        var products = component.get("v.products");
-        var product = products.find(p => p.Id === productId);
-        component.set("v.newProduct", JSON.parse(JSON.stringify(product)));
-    },
-
     deleteProduct: function(component, event, helper) {
-        var productId = event.currentTarget.dataset.recordId;
-        var action = component.get("c.deleteProduct");
-        action.setParams({ productId: productId });
+        var recordId = event.currentTarget.dataset.recordid;
+
+        var action = component.get("c.deleteProductRecord");
+        action.setParams({
+            productId: recordId
+        });
+
         action.setCallback(this, function(response) {
             var state = response.getState();
-            if (state === "SUCCESS") {
-                component.set("v.products", response.getReturnValue());
+            if(state == "SUCCESS"){
+                var products = component.get("v.products");
+                var updatedProducts = products.filter(function(product) {
+                    return product.Id !== recordId;
+                });
+                component.set("v.products", updatedProducts);
+                var resultsToast = $A.get("e.force:showToast");
+                            resultsToast.setParams({
+                                "title": "Record deleted",
+                                "variant": "success"
+                            });
+                            resultsToast.fire();
             } else {
-                console.error("Failed to delete product.");
+                var resultsToast = $A.get("e.force:showToast");
+                            resultsToast.setParams({
+                                "title": "Error during deleting record",
+                                "variant": "error"
+                            });
+                            resultsToast.fire();
+                console.log("Error deleting record " + state);
             }
         });
         $A.enqueueAction(action);
     },
-
-    saveProduct: function(component, event, helper) {
-        var newProduct = component.get("v.newProduct");
-        var action = component.get("c.saveProduct");
-        action.setParams({ product: newProduct });
-        action.setCallback(this, function(response) {
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                component.set("v.products", response.getReturnValue());
-                component.set("v.newProduct", { 'sobjectType': 'Product2' });
-            } else {
-                console.error("Failed to save product.");
-            }
-        });
-        $A.enqueueAction(action);
-    }
-});
+    handleError: function (cmp, event, helper) {
+            cmp.find('notifLib').showToast({
+                "title": "Something has gone wrong!",
+                "message": event.getParam("message"),
+                "variant": "error"
+            });
+        },
+    handleSuccess: function (cmp, event, helper) {
+            cmp.find('notifLib').showToast({
+                "title": "action successful!",
+                "message": event.getParam("message"),
+                "variant": "success"
+            });
+        }
+})
