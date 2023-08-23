a6a4f5b4ac943fe0bfba967cd799b5c83c2bffdc
Dawid Żamojda <dawidzamojdamail@wp.pl>
2023-08-23 11:36:34 +0200

    Finished frontend for discount manager

--- force-app/main/default/lwc/disountManager/disountManager.js
+++ force-app/main/default/lwc/disountManager/disountManager.js
@@ -7,14 +7,27 @@ import errorTitle from '@salesforce/label/c.Something_Wrong';
 export default class DisountManager extends LightningElement {
     allProducts = [];
     allCategories = [];
+    selectedWeekdays = [];
     selectedProducts = [];
     selectedCategories = [];
     @track productValue = '';
     @track promotionValue = '';
+    @track promotionValueType = '';
     @track selectProduct = false;
     @track selectCategory = false;
     @track oneTimePromotion = false;
     @track periodicPromotion = false;
+    @track periodicPromotionType = '';
+    @track periodicWeeklyPromotion = false;
+    @track periodicMonthlyPromotion = false;
+    @track percentagePromotionType = false;
+    @track absolutePromotionType = false;
+    @track absolutePromotionValue = 0;
+    @track percentagePromotionValue = 0;
+    @track oneTimePromotionStartDate = '';
+    @track oneTimePromotionEndDate = '';
+    @track periodicPromotionStartDate = '';
+    @track periodicPromotionEndDate = '';
 
     label = {
         errorTitle
@@ -25,6 +38,32 @@ export default class DisountManager extends LightningElement {
             {label: 'Product', value: 'product'},
             {label: 'Category', value: 'category'}
         ];
+    }
+
+    get promotionValueTypeOptions() {
+            return [
+                {label: 'Percent', value: 'percent'},
+                {label: 'Absolute', value: 'absolute'}
+            ];
+        }
+
+    get periodicPromotionOptions() {
+            return [
+                {label: 'Weekly', value: 'weekly'},
+                {label: 'Monthly', value: 'monthly'}
+            ];
+        }
+
+    get weekdayOptions() {
+        return [
+            {label: 'Monday', value: 'monday'},
+            {label: 'Tuesday', value: 'tuesday'},
+            {label: 'Wednesday', value: 'wednesday'},
+            {label: 'Thursday', value: 'thursday'},
+            {label: 'Friday', value: 'friday'},
+            {label: 'Saturday', value: 'saturday'},
+            {label: 'Sunday', value: 'sunday'},
+        ]
     }
 
     get promotionOptions() {
@@ -63,10 +102,12 @@ export default class DisountManager extends LightningElement {
 
     handleProductCheckboxChange(event) {
         this.selectedProducts = event.detail.value;
+        console.log(this.selectedProducts);
     }
 
     handleCategoryCheckboxChange(event) {
         this.selectedCategories = event.detail.value;
+        console.log(this.selectedCategories);
     }
 
     handleProductSelectionChange(event) {
@@ -74,6 +115,7 @@ export default class DisountManager extends LightningElement {
 
         this.selectProduct = this.productValue === 'product';
         this.selectCategory = this.productValue === 'category';
+        console.log(this.productValue);
     }
 
     handlePromotionSelectionChange(event) {
@@ -81,6 +123,28 @@ export default class DisountManager extends LightningElement {
 
         this.oneTimePromotion = this.promotionValue === 'oneTime';
         this.periodicPromotion = this.promotionValue === 'periodic';
+        console.log(this.promotionValue);
+    }
+
+    handlePeriodicPromotionTypeChange(event) {
+        this.periodicPromotionType = event.detail.value;
+
+        this.periodicWeeklyPromotion = this.periodicPromotionType === 'weekly';
+        this.periodicMonthlyPromotion = this.periodicPromotionType === 'monthly';
+        console.log(this.periodicPromotionType);
+    }
+
+    handleWeekdaysCheckboxChange(event) {
+        this.selectedWeekdays = event.detail.value;
+        console.log(this.selectedWeekdays);
+    }
+
+    handlePromotionValueTypeChange(event) {
+        this.promotionValueType = event.detail.value;
+        console.log(this.promotionValueType);
+
+        this.absolutePromotionType = this.promotionValueType === 'percent';
+        this.percentagePromotionType = this.promotionValueType === 'absolute';
     }
 
     displayToast(title, variant) {
