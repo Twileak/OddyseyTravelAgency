a49c47f53711b5b7c249f08eba4f24e1d7c8b35c
Dawid Żamojda <dawidzamojdamail@wp.pl>
2023-09-19 11:32:49 +0200

    Added Labels and Trigger Factory

--- force-app/main/default/lwc/currencyPicker/currencyPicker.js
+++ force-app/main/default/lwc/currencyPicker/currencyPicker.js
@@ -2,6 +2,7 @@ import { LightningElement, track, wire, api} from 'lwc';
 import { createRecord } from 'lightning/uiRecordApi';
 import updateCurrency from '@salesforce/apex/CurrencyPickerController.updateCurrency';
 import getCurrentCurrency from '@salesforce/apex/CurrencyPickerController.selectCurrentCurrency';
+import getSupportedCurrencies from '@salesforce/apex/CurrencyPickerController.getSupportedCurrencies';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import errorTitle from '@salesforce/label/c.Something_Wrong';
 import PLN from '@salesforce/label/c.PLN';
@@ -10,6 +11,7 @@ import USD from '@salesforce/label/c.USD';
 
 export default class CurrencyUpdateLWC extends LightningElement {
     @track selectedCurrency;
+    currencyOptions = [];
 
     label = {
             errorTitle,
@@ -17,13 +19,17 @@ export default class CurrencyUpdateLWC extends LightningElement {
             USD
         }
 
-    currencyOptions = [
-        { label: this.label.PLN, value: 'PLN' },
-        { label: this.label.USD, value: 'USD' }
-    ];
-
-
-
+    @wire(getSupportedCurrencies)
+    wiredSupportedCurrencies({data, error}) {
+        if(data) {
+            this.currencyOptions = data.split(';').map((currency) => ({
+                label: currency,
+                value: currency
+            }));
+        } else if(error) {
+            this.displayToast(this.label.errorTitle, JSON.stringify(error), "error");
+        }
+    }
 
     @wire(getCurrentCurrency)
     wiredReviews({error, data}) {
--- force-app/main/default/triggers/StoreCurrencyUpdate.trigger
+++ force-app/main/default/triggers/StoreCurrencyUpdate.trigger
@@ -1,39 +1,5 @@
 trigger StoreCurrencyUpdate on Edit_Default_Currency__e (after insert) {
-
-    Set<Id> storeIds = new Set<Id>();
-
-    Map<Id, String> storeIdToNewCurrencyMap = new Map<Id, String>();
-
-    WebStore store = [
-            SELECT Name, SupportedCurrencies
-            FROM WebStore
-            WHERE Name = 'Oddysey'];
-
-    List<String> supportedCurrencies = store.SupportedCurrencies.split(';');
-
-    for (Edit_Default_Currency__e event : Trigger.new) {
-        if (supportedCurrencies.contains(event.NewCurrency__c)) {
-            storeIds.add(event.Store_Id__c);
-            storeIdToNewCurrencyMap.put(event.Store_Id__c, event.NewCurrency__c);
-        }
-    }
-
-    Map<Id, WebStore> webStoresMap = new Map<Id, WebStore>([
-            SELECT Id, Name, CurrencyIsoCode
-            FROM WebStore
-            WHERE Id IN :storeIds
-    ]);
-
-    List<WebStore> webStoresToUpdate = new List<WebStore>();
-    for (Id storeId : storeIds) {
-        if (webStoresMap.containsKey(storeId)) {
-            WebStore webStore = webStoresMap.get(storeId);
-            webStore.CurrencyIsoCode = storeIdToNewCurrencyMap.get(storeId);
-            webStoresToUpdate.add(webStore);
-        }
-    }
-
-    if (!webStoresToUpdate.isEmpty()) {
-        update webStoresToUpdate;
+    if(CheckRecursive.runOnce()){
+        TriggerDispatcher.run(new PlatformEventTriggerHandler());
     }
 }
